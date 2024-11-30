91c329606809e05caa5cf99eabd56c8761f8a87f
raybac <hponsin@gmail.com>
2024-11-30 21:31:34 +0100

    Initial web page

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Cooki-tech
 Les gâteaux, c'est sacré ! Et avec Cooki-tech, on s'en souvient.
+
+
+#Lancement : 
+
+    $   npm start
+
+# Organisation :
+
+/public ==> tout ce que le client a acces directement
+
+
---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>
+	{
+		res.sendFile(path.join(__dirname, 'public', 'main.html'));
+	});
+
+
+app.listen(port, () =>
+	{
+		console.log(`Operating on : http://localhost:${port}`);
+	});
